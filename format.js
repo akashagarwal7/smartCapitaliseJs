da94a6271b3a9ea3e98beba90e5deb6b1ac8454b
Akash Aggarwal <virusofminds@gmail.com>
2017-09-05 14:59:50 +0530

    Hopefully fixed page reloading on github on form submission

--- format.js
+++ format.js
@@ -1,5 +1,5 @@
-function format(argument) {
-	var strr = argument.ta1.value.toLowerCase();
+document.getElementById('my-form').onsubmit = function() {
+	var strr = document.getElementById("ta1").value.toLowerCase();
 	console.log (strr);
 	var promise = new RSVP.Promise(function(fulfill, reject) {
 		strr = strr.replace(/(^.)|(\si(\s|\'))|(\.\s[a-z])/gm, function (match) { 
